c0cb1513e3ac1d958705ac918a6e16ed8609763d
szidimisi68 <misikek5@gmail.com>
2023-05-25 11:50:08 +0200

    1. Allapot

---
+++ Kutya.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp8
+{
+    internal class Kutya
+    {
+        int id;
+        string fajta;
+        string nev;
+        int eletkor;
+        string datum;
+
+        public Kutya(string adatsor)
+        {
+            string[] felbontottAdatsor = adatsor.Split(";");
+            this.id = int.Parse(felbontottAdatsor[0]);
+            this.fajta = felbontottAdatsor[1];
+            this.nev = felbontottAdatsor[2];
+            this.eletkor = int.Parse(felbontottAdatsor[3]);
+            this.datum = felbontottAdatsor[4];
+        }
+
+
+        public int Id { get => id; set => id = value; }
+        public string Fajta { get => fajta; set => fajta = value; }
+        public string Nev { get => nev; set => nev = value; }
+        public int Eletkor { get => eletkor; set => eletkor = value; }
+        public string Datum { get => datum; set => datum = value; }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -6,8 +6,29 @@ namespace ConsoleKutya
     {
         static void Main(string[] args)
         {
-            
+            List<string[]> nevek = new List<string[]>();
+            StreamReader sr = new StreamReader("D:\\C sharp\\ConsoleApp8\\datas\\KutyaNevek.csv");
+            sr.ReadLine();
+            while (!sr.EndOfStream)
+            {
+                string sor = sr.ReadLine();
+                nevek.Add(sor.Split(";"));
+            }
+            sr.Close();
 
+
+            List<string[]> fajtak = new List<string[]>();
+            sr = new StreamReader("D:\\C sharp\\ConsoleApp8\\datas\\KutyaFajtak.csv");
+            sr.ReadLine();
+            while (!sr.EndOfStream)
+            {
+                string sor = sr.ReadLine();
+                fajtak.Add(sor.Split(";"));
+            }
+            sr.Close();
+
+
+            Console.WriteLine($"3.feladat: Kutyanevek száma: {nevek.Count()}");
         }
     }
 }
